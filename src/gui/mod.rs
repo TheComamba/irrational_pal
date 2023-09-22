099050cc37150a46f53cc7c8fa12e524a21d4d6d
TheComamba <thecomamba@jojoheinze.de>
2023-09-22 07:34:02 +0200

    Basic gui.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,5 +1,5 @@
 use iced::{
-    widget::{Column, Text},
+    widget::{Button, Column, Row, Text},
     Sandbox,
 };
 
@@ -13,12 +13,48 @@ impl Sandbox for Gui {
     }
 
     fn title(&self) -> String {
-        String::from("Hello, world!")
+        String::from("Irrational Pal")
     }
 
     fn update(&mut self, _message: Self::Message) {}
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        Column::new().push(Text::from("value")).into()
+        let button_e = Button::new(Text::new("e"));
+        let button_pi = Button::new(Text::new("Pi"));
+        let button_browse = Button::new(Text::new("Browse"));
+        let button_recite = Button::new(Text::new("Recite"));
+        Column::new()
+            .push(
+                Row::new()
+                    .push(button_e)
+                    .push(button_pi)
+                    .padding(5)
+                    .spacing(5),
+            )
+            .push(
+                Row::new()
+                    .push(button_browse)
+                    .push(button_recite)
+                    .padding(5)
+                    .spacing(5),
+            )
+            .push(self.digit_view())
+            .padding(5)
+            .spacing(5)
+            .into()
+    }
+}
+
+impl Gui {
+    fn digit_view(&self) -> iced::Element<'_, ()> {
+        let mut row = Row::new();
+        let button_left = Button::new(Text::new("<"));
+        let button_right = Button::new(Text::new(">"));
+        row = row.push(button_left);
+        for i in 0..10 {
+            row = row.push(Text::new(format!("{}", i)));
+        }
+        row = row.push(button_right);
+        row.padding(5).spacing(5).into()
     }
 }
