51f614034ac26092321b049f9927ae7c7cabf360
TheComamba <thecomamba@jojoheinze.de>
2023-09-22 06:56:49 +0200

    Adjusted main.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use data::e;
+
 mod data;
 
 fn main() {
-    println!("Hello, world!");
+    let digits_e = data::extraction::get_digits(&e::E, 0, 10);
+    for i in 0..10 {
+        println!("{}: {}", i, digits_e[i]);
+    }
+
+    let digits_pi = data::extraction::get_digits(&data::pi::PI, 0, 10);
+    for i in 0..10 {
+        println!("{}: {}", i, digits_pi[i]);
+    }
 }
