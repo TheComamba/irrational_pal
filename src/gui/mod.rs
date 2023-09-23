a47ad35db256105b29cfad9a82934bfcdb99f268
TheComamba <thecomamba@jojoheinze.de>
2023-09-23 10:13:28 +0200

    Enable picking a number.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,50 +3,59 @@ use iced::{
     Sandbox,
 };
 
-pub(crate) struct Gui {}
+use crate::data::{e::E, pi::PI};
+
+pub(crate) struct Gui {
+    number: Option<&'static str>,
+}
 
 impl Sandbox for Gui {
-    type Message = ();
+    type Message = GuiMessage;
 
     fn new() -> Self {
-        Gui {}
+        Gui { number: None }
     }
 
     fn title(&self) -> String {
         String::from("Irrational Pal")
     }
 
-    fn update(&mut self, _message: Self::Message) {}
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            GuiMessage::PickedNumber(number) => {
+                self.number = Some(number);
+            }
+        }
+    }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        let button_e = Button::new(Text::new("e"));
-        let button_pi = Button::new(Text::new("Pi"));
+        let button_e = Button::new(Text::new("e")).on_press(GuiMessage::PickedNumber(E));
+        let button_pi = Button::new(Text::new("Pi")).on_press(GuiMessage::PickedNumber(PI));
         let button_browse = Button::new(Text::new("Browse"));
         let button_recite = Button::new(Text::new("Recite"));
-        Column::new()
-            .push(
-                Row::new()
-                    .push(button_e)
-                    .push(button_pi)
-                    .padding(5)
-                    .spacing(5),
-            )
-            .push(
+        let mut col = Column::new().push(
+            Row::new()
+                .push(button_e)
+                .push(button_pi)
+                .padding(5)
+                .spacing(5),
+        );
+        if self.number.is_some() {
+            col = col.push(
                 Row::new()
                     .push(button_browse)
                     .push(button_recite)
                     .padding(5)
                     .spacing(5),
-            )
-            .push(self.digit_view())
-            .padding(5)
-            .spacing(5)
-            .into()
+            );
+            col = col.push(self.digit_view());
+        }
+        col.padding(5).spacing(5).into()
     }
 }
 
 impl Gui {
-    fn digit_view(&self) -> iced::Element<'_, ()> {
+    fn digit_view(&self) -> iced::Element<'_, GuiMessage> {
         let mut row = Row::new();
         let button_left = Button::new(Text::new("<"));
         let button_right = Button::new(Text::new(">"));
@@ -57,4 +66,9 @@ impl Gui {
         row = row.push(button_right);
         row.padding(5).spacing(5).into()
     }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) enum GuiMessage {
+    PickedNumber(&'static str),
 }
