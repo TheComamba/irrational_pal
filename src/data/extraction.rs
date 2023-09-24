aec75b78faf263e2299f6ba4fcdc9408a5239b3d
TheComamba <thecomamba@jojoheinze.de>
2023-09-24 17:40:26 +0200

    Refactoring number representation into separate file.

--- src/data/extraction.rs
+++ src/data/extraction.rs
@@ -1,4 +1,5 @@
 use super::Number;
+use std::cmp::min;
 
 pub(crate) fn get_digits(number: &Number, pos: u32, amount: u32) -> Vec<char> {
     let mut digits: Vec<char> = Vec::new();
@@ -10,4 +11,42 @@ pub(crate) fn get_digits(number: &Number, pos: u32, amount: u32) -> Vec<char> {
         count += 1;
     }
     digits
+}
+
+pub(crate) fn number_representation(number: &Number, pos: u32) -> String {
+    let name = number.name;
+    let amount = min(pos, 10);
+    let start = pos - amount;
+    let mut digits = get_digits(number, start, amount);
+    if start == 0 && !digits.is_empty() {
+        digits.insert(1, '.');
+    } else if start > 0 {
+        for _ in 0..3 {
+            digits.insert(0, '.');
+        }
+    }
+    let digits = digits.iter().collect::<String>();
+    format!("{}={}", name, digits)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::data::e::E;
+
+    #[test]
+    fn test_number_text() {
+        assert_eq!(number_representation(&E, 0), "e=");
+        assert_eq!(number_representation(&E, 1), "e=2.");
+        assert_eq!(number_representation(&E, 2), "e=2.7");
+        assert_eq!(number_representation(&E, 3), "e=2.71");
+        assert_eq!(number_representation(&E, 4), "e=2.718");
+        assert_eq!(number_representation(&E, 5), "e=2.7182");
+        assert_eq!(number_representation(&E, 6), "e=2.71828");
+        assert_eq!(number_representation(&E, 7), "e=2.718281");
+        assert_eq!(number_representation(&E, 8), "e=2.7182818");
+        assert_eq!(number_representation(&E, 9), "e=2.71828182");
+        assert_eq!(number_representation(&E, 10), "e=2.718281828");
+        assert_eq!(number_representation(&E, 11), "e=...7182818284");
+    }
 }
--- src/gui/recite.rs
+++ src/gui/recite.rs
@@ -1,39 +1,20 @@
-use std::cmp::min;
-
 use super::{Gui, GuiMessage};
-use crate::data::extraction::get_digits;
+use crate::data::extraction::number_representation;
 use iced::widget::{Button, Column, Row, Text};
 
 impl Gui {
     pub(super) fn recite_view(&self) -> iced::Element<'_, GuiMessage> {
+        let number_str = match self.number {
+            Some(n) => number_representation(n, self.recite_pos),
+            None => String::from(""),
+        };
         Column::new()
             .push(Text::new(format!("Next position: {}", self.recite_pos)))
-            .push(Text::new(self.recited_view()))
+            .push(Text::new(number_str))
             .push(self.digit_buttons())
             .padding(5)
             .spacing(5)
             .into()
-    }
-
-    fn recited_view(&self) -> String {
-        let number = match self.number {
-            Some(n) => n,
-            None => return String::from(""),
-        };
-
-        let name = number.name;
-        let amount = min(self.recite_pos, 10);
-        let start = self.recite_pos - amount;
-        let mut recited_digits = get_digits(number, start, amount);
-        if start == 0 && !recited_digits.is_empty() {
-            recited_digits.insert(1, '.');
-        } else if start > 0 {
-            for _ in 0..3 {
-                recited_digits.insert(0, '.');
-            }
-        }
-        let recited_digits = recited_digits.iter().collect::<String>();
-        format!("{}={}", name, recited_digits)
     }
 
     fn digit_buttons(&self) -> iced::Element<'_, GuiMessage> {
@@ -47,42 +28,5 @@ impl Gui {
             row = row.push(button);
         }
         row.padding(5).spacing(5).into()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::data::e::E;
-    use iced::Sandbox;
-
-    #[test]
-    fn test_number_text() {
-        let mut gui = Gui::new();
-        gui.number = Some(&E);
-        gui.recite_pos = 0;
-        assert_eq!(gui.recited_view(), "e=");
-        gui.recite_pos = 1;
-        assert_eq!(gui.recited_view(), "e=2.");
-        gui.recite_pos = 2;
-        assert_eq!(gui.recited_view(), "e=2.7");
-        gui.recite_pos = 3;
-        assert_eq!(gui.recited_view(), "e=2.71");
-        gui.recite_pos = 4;
-        assert_eq!(gui.recited_view(), "e=2.718");
-        gui.recite_pos = 5;
-        assert_eq!(gui.recited_view(), "e=2.7182");
-        gui.recite_pos = 6;
-        assert_eq!(gui.recited_view(), "e=2.71828");
-        gui.recite_pos = 7;
-        assert_eq!(gui.recited_view(), "e=2.718281");
-        gui.recite_pos = 8;
-        assert_eq!(gui.recited_view(), "e=2.7182818");
-        gui.recite_pos = 9;
-        assert_eq!(gui.recited_view(), "e=2.71828182");
-        gui.recite_pos = 10;
-        assert_eq!(gui.recited_view(), "e=2.718281828");
-        gui.recite_pos = 11;
-        assert_eq!(gui.recited_view(), "e=...7182818284");
     }
 }
