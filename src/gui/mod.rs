e87335a8015d24b5b79dfaa0c9ec51f29c397cc3
TheComamba <thecomamba@jojoheinze.de>
2023-09-24 06:34:57 +0200

    Enabled picking a mode.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -7,13 +7,19 @@ use crate::data::{e::E, pi::PI};
 
 pub(crate) struct Gui {
     number: Option<&'static str>,
+    mode: GuiMode,
+    pos: u32,
 }
 
 impl Sandbox for Gui {
     type Message = GuiMessage;
 
     fn new() -> Self {
-        Gui { number: None }
+        Gui {
+            number: None,
+            mode: GuiMode::Browse,
+            pos: 0,
+        }
     }
 
     fn title(&self) -> String {
@@ -22,17 +28,19 @@ impl Sandbox for Gui {
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            GuiMessage::PickedNumber(number) => {
-                self.number = Some(number);
-            }
+            GuiMessage::PickedNumber(number) => self.number = Some(number),
+
+            GuiMessage::PickedMode(mode) => self.mode = mode,
         }
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
         let button_e = Button::new(Text::new("e")).on_press(GuiMessage::PickedNumber(E));
         let button_pi = Button::new(Text::new("Pi")).on_press(GuiMessage::PickedNumber(PI));
-        let button_browse = Button::new(Text::new("Browse"));
-        let button_recite = Button::new(Text::new("Recite"));
+        let button_browse =
+            Button::new(Text::new("Browse")).on_press(GuiMessage::PickedMode(GuiMode::Browse));
+        let button_recite =
+            Button::new(Text::new("Recite")).on_press(GuiMessage::PickedMode(GuiMode::Recite));
         let mut col = Column::new().push(
             Row::new()
                 .push(button_e)
@@ -48,7 +56,12 @@ impl Sandbox for Gui {
                     .padding(5)
                     .spacing(5),
             );
-            col = col.push(self.digit_view());
+            match self.mode {
+                GuiMode::Browse => {
+                    col = col.push(self.digit_view());
+                }
+                GuiMode::Recite => {}
+            }
         }
         col.padding(5).spacing(5).into()
     }
@@ -71,4 +84,11 @@ impl Gui {
 #[derive(Debug, Clone)]
 pub(crate) enum GuiMessage {
     PickedNumber(&'static str),
+    PickedMode(GuiMode),
+}
+
+#[derive(Debug, Clone)]
+pub(crate) enum GuiMode {
+    Browse,
+    Recite,
 }
